5313ec1a5a4dabfe992018460a171403e93df242
Mike Doan <manhdoanmainmail@gmail.com>
2024-03-06 17:24:54 -0500

    update to chatpage

--- flavorfeed/pages/chat/index.js
+++ flavorfeed/pages/chat/index.js
@@ -1,28 +1,16 @@
 import styles from '../../styles/Home.module.css';
 import { useEffect, useState } from 'react';
-import Card from './card';
+import Card from './Card';
+import ChatContainer from './ChatContainer';
 
 export default function Home() {
   const [chatLogs, setChatLogs] = useState([]);
-  const [message, setMessage] = useState('');
 
   useEffect(() => {
     fetch('/chat.json')
         .then(response => response.json())
         .then(setChatLogs);
     }, []);
-
-  const sendMessage = async () => {
-    const response = await fetch('/api/send_message', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username: 'user', message }),
-    });
-
-    const data = await response.json();
-    setChatLogs([...chatLogs, data]);
-    setMessage('');
-  };
 
   return (
     <div className={styles.container}>
@@ -31,22 +19,7 @@ export default function Home() {
         <Card />
       </div>
 
-      <div id="chat-container" style={{position: 'fixed', bottom: '0', right: '0'}}>
-        {chatLogs.map((log, index) => (
-          <div key={index} className={log.username === 'user' ? 'user-message' : 'system-message'}>
-            <p>{log.message}</p>
-          </div>
-        ))}
-
-        <div id="message-input">
-          <input
-            type="text"
-            value={message}
-            onChange={e => setMessage(e.target.value)}
-          />
-          <button onClick={sendMessage}>Send</button>
-        </div>
-      </div>
+      <ChatContainer chatLogs={chatLogs} setChatLogs={setChatLogs} />
     </div>
   );
 }
