15ab9810ca1e387645ac5411060e6b42966c7426
Ramita Rathore <ramitarathore@Ramitas-MBP.wifi.gsu.edu>
2024-03-07 12:47:03 -0500

    u

--- flavorfeed/pages/FilterPages/index.js
+++ flavorfeed/pages/FilterPages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Made by ByteSizedBrunchers
-	  Last Updated on 3/7 7:06 am
+	  Last Updated on 3/7 12:46 pm
         </a>
       </footer>
 
