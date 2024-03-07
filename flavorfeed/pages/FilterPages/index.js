be3590e099a52e7b8fe6f68595a87aabe5e49fd9
Ramita Rathore <ramitarathore@Ramitas-MBP.attlocal.net>
2024-03-07 05:00:17 -0500

    u

--- flavorfeed/pages/FilterPages/index.js
+++ flavorfeed/pages/FilterPages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 	<p>Use these filters to find exactly what you are looking for?</p>
 	<search>
 	<label>Filter:</label>
-	<input type="search" placeholder="get closer to your perfect recipe" name="filter">
+	<input type="search" placeholder="get closer to your perfect recipe" name="filter"/>
 	</search>
       </main>
 
