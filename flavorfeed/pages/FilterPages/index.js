5d6f8b1bf9a71537dad0b1433f50ec7aef44e4a5
Ramita Rathore <ramitarathore@Ramitas-MBP.attlocal.net>
2024-03-07 05:08:41 -0500

    u

--- flavorfeed/pages/FilterPages/index.js
+++ flavorfeed/pages/FilterPages/index.js
@@ -38,8 +38,8 @@ export default function Home() {
         }
 
 	search {
-          width: 100%;
-          height: 500px;
+          width: 200%;
+          height: 100px;
           border-top: 1px solid #eaeaea;
           display: flex;
           justify-content: center;
