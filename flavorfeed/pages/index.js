113aceacc41f4209c42e4a6f7eb7724c2f9527ec
Ramita Rathore <ramitarathore@Ramitas-MBP.wifi.gsu.edu>
2024-04-02 16:33:28 -0400

    woo

--- flavorfeed/pages/index.js
+++ flavorfeed/pages/index.js
@@ -1,6 +1,6 @@
 //limport Head from 'next/head';
 import styles from '../styles/Home.module.css';
-
+import Link from 'next/link';
 export default function Home() {
   return (
     <div className={styles.container}>
