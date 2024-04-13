f5ae86251550618210c3a0568e98585705d6cfae
ramitarathore <145944760+ramitarathore@users.noreply.github.com>
2024-04-13 03:28:49 -0400

    Update index.js

--- flavorfeed/pages/FilterPages/index.js
+++ flavorfeed/pages/FilterPages/index.js
@@ -35,6 +35,7 @@ export default function Home() {
 	<option value = "Non-Vegeterian"> Non-Vegeterian </option>
 	<option value = "Vegeterian"> Vegeterian </option>
 	<option value = "Eggterian"> Eggterian </option>
+	<option value = "Pescatarian"> Pescatarian </option>
 	</select>
 
       </main>
