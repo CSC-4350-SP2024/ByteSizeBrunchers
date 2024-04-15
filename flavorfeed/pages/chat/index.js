6088e9cfe4f62680564e4ea6db3e96ed62f053ad
ramitarathore <145944760+ramitarathore@users.noreply.github.com>
2024-04-15 00:04:08 -0400

    Chatpage Updates (#5)
    * adding index.js to the chatpage
    
    * adding stylesheet for chat page chatpage.module.css
    
    * updating index.js
    
    * updating index.js
    
    * syncing branch
    
    ---------
    
    Co-authored-by: Ramita Rathore <ramitarathore@Ramitas-MBP.attlocal.net>

--- flavorfeed/pages/chat/index.js
+++ flavorfeed/pages/chat/index.js
@@ -1,29 +1,122 @@
+import Head from 'next/head';
+import mainpage from '../index.js';
 import dynamic from 'next/dynamic'; //debugging entry
-import styles from '../../styles/Home.module.css';
+import styles from './chatpage.module.css';
 import { useEffect, useState } from 'react';
-import ChatContainer from './ChatContainer';
-
-//dynamic import of Card component with SSR disabled
-const CardWithNoSSR = dynamic(() => import('./Card'), { ssr: false }); 
 
 export default function Home() {
-  const [chatLogs, setChatLogs] = useState([]);
 
-  useEffect(() => {
-    fetch('/chat.json')
-        .then(response => response.json())
-        .then(setChatLogs);
-    }, []);
+	const [messages, setMessages] = useState([]);
+	const server_url = 'http://127.0.0.1:3000/query';
 
-  return (
-    <div className={styles.container}>
+	async function ff_userinput(userinput) {
+		// Add user message to the chat window
+		setMessages([...messages, { text: userinput, from: 'user' }]);
+		
+		try {
+			// Make API call to LLM API using fetch
+			const response = await fetch(server_url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					userID: 'e6328e4a-4477-4273-b299-8630ee657439',
+					query: userinput
+				})
+			});
 
-      <div id="card" style={{position: 'absolute', top: '25%', left: '35%', transform: 'translate(-50%, -50%)', height: '80vh', width: '50vw'}}>
-        {/* <Card /> Commented out for debugging */}
-        <CardWithNoSSR />
-      </div>
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
 
-      <ChatContainer chatLogs={chatLogs} setChatLogs={setChatLogs} />
-    </div>
-  );
+			/* get llm response and add to list */
+			const llm_response = await response.json();
+			setMessages([...messages, { text: llm_response.choices[0].message.content, from: 'bot' }]);
+		} catch (error) {
+			console.error('Error fetching response from backend API:', error);
+		}
+	}
+
+	return (
+<div className={styles.container}>
+	<title>Flavor Feed Chatbot</title>	
+
+	<div className={styles.title}>
+		Flavor Feed
+	</div>	
+	<div className={styles.chatboxcontainer}>
+		<div className={styles.chatleft} />
+		<div className={styles.chatbox}>	
+			<ul className={styles.chatbot}>
+				<div height="500px"> 
+					<center> <img src="../ff_logo.jpg"/> </center>
+				</div>
+				<li className= {styles.outgoing}>
+					<span> <img src="../ff_logo.jpg" height="40px"/> </span>
+					<p> Hello! What recipe can I help you find today? </p>
+				</li>
+
+				{messages.map((message, index) => (
+					<li key={index} className={message.from === 'user' ? styles.incoming : styles.outgoing}>
+					{message.from === 'user' ? "" : (<span> <img src="../ff_logo.jpg" height="40px"/> </span>)}
+					<p>{message.text}</p>
+				</li>))}
+
+				
+			</ul>
+			<div className= {styles.chatbottom}>
+				<div className={styles.input}>
+					<input type="Start looking for recipes..." onKeyDown={(e) => e.key === 'Enter' && ff_userinput(e.target.value)} />
+				</div>
+				<div className={styles.buttonbox}>
+					<button type="button" className={styles.button}>Enter</button>
+				</div>
+			</div>
+		</div>
+		<div className={styles.chatright} />
+	</div>
+
+
+	<footer className={styles.footer}>
+		<a href="https://github.com/CSC-4350-SP2024/ByteSizeBrunchers" target="_blank" rel="noopener noreferrer">Made by ByteSizedBrunchers</a>
+	</footer>
+
+
+
+
+
+
+
+
+
+			<style jsx global>{`
+				html,
+				body {
+					padding: 0;
+					margin: 0;
+					font-family:
+						-apple-system,
+						BlinkMacSystemFont,
+						Segoe UI,
+						Roboto,
+						Oxygen,
+						Ubuntu,
+						Cantarell,
+						Fira Sans,
+						Droid Sans,
+						Helvetica Neue,
+						sans-serif;
+				}
+				* {
+					box-sizing: border-box;
+				}
+			`}</style>
+
+		</div>
+	);
 }
+
+
+
+	
